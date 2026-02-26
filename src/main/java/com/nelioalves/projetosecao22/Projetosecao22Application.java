fbc37475ebf46e2c8e4cf83fc082d1164875b725
bartguitar <adrielbart@gmail.com>
2026-02-26 10:32:40 -0300

    Project created

---
+++ src/main/java/com/nelioalves/projetosecao22/Projetosecao22Application.java
@@ -0,0 +1,13 @@
+package com.nelioalves.projetosecao22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetosecao22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetosecao22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/projetosecao22/Projetosecao22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.projetosecao22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetosecao22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
