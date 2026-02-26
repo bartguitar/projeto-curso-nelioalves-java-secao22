487880622b8df96f8ad2e4acdbb9cd5cc60e5e79
bartguitar <adrielbart@gmail.com>
2026-02-26 12:15:11 -0300

    Service layer, component registration

---
+++ src/main/java/com/nelioalves/projetosecao22/services/UserService.java
@@ -0,0 +1,28 @@
+package com.nelioalves.projetosecao22.services;
+
+
+import com.nelioalves.projetosecao22.entities.User;
+import com.nelioalves.projetosecao22.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
