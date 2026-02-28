59fdff85d94e43e729519920dcd730322d3280ee
bartguitar <adrielbart@gmail.com>
2026-02-28 10:49:43 -0300

    Exception handling - update

--- src/main/java/com/nelioalves/projetosecao22/services/UserService.java
+++ src/main/java/com/nelioalves/projetosecao22/services/UserService.java
@@ -6,6 +6,7 @@ import com.nelioalves.projetosecao22.repositories.UserRepository;
 
 import com.nelioalves.projetosecao22.services.exceptions.DatabaseException;
 import com.nelioalves.projetosecao22.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     public void updateData(User entity, User obj){
