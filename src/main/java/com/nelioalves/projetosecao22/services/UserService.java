86289049e0f33143a0861a6d478262fa108a40a0
bartguitar <adrielbart@gmail.com>
2026-02-28 10:45:31 -0300

    Exception handling - delete

--- src/main/java/com/nelioalves/projetosecao22/services/UserService.java
+++ src/main/java/com/nelioalves/projetosecao22/services/UserService.java
@@ -4,8 +4,11 @@ package com.nelioalves.projetosecao22.services;
 import com.nelioalves.projetosecao22.entities.User;
 import com.nelioalves.projetosecao22.repositories.UserRepository;
 
+import com.nelioalves.projetosecao22.services.exceptions.DatabaseException;
 import com.nelioalves.projetosecao22.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -33,7 +36,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
